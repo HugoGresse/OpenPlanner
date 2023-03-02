cd968261938a38e8cb1820be0b26c58e8ba3b591
Hugo Gresse <hugo.gresse@gmail.com>
2023-03-02 15:59:27 +0100

    Aaaaaaa

--- src/events/page/EventLayout.tsx
+++ src/events/page/EventLayout.tsx
@@ -3,18 +3,26 @@ import { useState } from 'react'
 import {
     AppBar as MuiAppBar,
     AppBarProps as MuiAppBarProps,
+    Avatar,
     Box,
+    Divider,
     Drawer as MuiDrawer,
     IconButton,
+    List,
+    ListItem,
+    ListItemAvatar,
+    ListItemText,
     styled,
     Toolbar,
     Typography,
 } from '@mui/material'
-import { Menu } from './EventScreenMenuItems'
+import { EventScreenMenuItems, Menu } from './EventScreenMenuItems'
 import MenuIcon from '@mui/icons-material/Menu'
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft'
 import { useRoute } from 'wouter'
 import { useSelector } from 'react-redux'
-import { selectUserConferenceCenter } from '../../auth/authReducer'
+import { logout, selectUserConferenceCenter } from '../../auth/authReducer'
+import LogoutIcon from '@mui/icons-material/Logout'
 import { useAppDispatch } from '../../reduxStore'
 
 const drawerWidth: number = 240
@@ -104,6 +112,38 @@ export const EventLayout = ({ children }: EventLayoutProps) => {
                     </Typography>
                 </Toolbar>
             </AppBar>
+            <Drawer variant="permanent" open={open}>
+                <Toolbar
+                    sx={{
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'flex-end',
+                        px: [1],
+                    }}>
+                    <IconButton onClick={toggleDrawer}>
+                        <ChevronLeftIcon />
+                    </IconButton>
+                </Toolbar>
+                <Divider />
+                <List component="nav">
+                    <EventScreenMenuItems />
+                </List>
+                <Box marginTop="auto">
+                    <List>
+                        <ListItem
+                            secondaryAction={
+                                <IconButton edge="end" aria-label="logout" onClick={() => dispatch(logout())}>
+                                    <LogoutIcon />
+                                </IconButton>
+                            }>
+                            <ListItemAvatar>
+                                <Avatar alt={user?.avatarURL} src={user?.displayName}></Avatar>
+                            </ListItemAvatar>
+                            <ListItemText primary={user?.displayName} />
+                        </ListItem>
+                    </List>
+                </Box>
+            </Drawer>
         </Box>
     )
 }
