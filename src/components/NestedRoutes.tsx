26ca14e68af2ed88c4075cc176781367c22f61a8
Hugo Gresse <hugo.gresse@gmail.com>
2023-03-02 16:11:02 +0100

    Aaaaaaa

--- src/components/NestedRoutes.tsx
+++ src/components/NestedRoutes.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { useLocation, useRouter } from 'wouter'
+import { Router, useLocation, useRouter } from 'wouter'
 
 // from https://github.com/molefrog/wouter#are-relative-routes-and-links-supported
 export const NestedRoutes = (props: { children: React.ReactNode; base: string }) => {
@@ -11,6 +11,12 @@ export const NestedRoutes = (props: { children: React.ReactNode; base: string })
     // don't render anything outside of the scope
     if (!parentLocation.startsWith(nestedBase)) return null
 
+    console.log('render subroute')
+
     // we need key to make sure the router will remount when base changed
-    return null
+    return (
+        <Router base={nestedBase} key={nestedBase}>
+            {props.children}
+        </Router>
+    )
 }
