fb04a4178b0a4d3ff769cf73252f8ee1b5a1001b
Hugo Gresse <hugo.gresse@gmail.com>
2023-03-03 17:53:34 +0100

    Add settings edition (name, tracks, urls)
    * WIP
    
    * Add settings update (without date) + rollback react-query-firebase to fix invalidate not working

--- src/events/page/EventSponsors.tsx
+++ src/events/page/EventSponsors.tsx
@@ -1,15 +1,10 @@
 import * as React from 'react'
-import { useRoute } from 'wouter'
 
 export type EventSponsorsProps = {}
 export const EventSponsors = (props: EventSponsorsProps) => {
-    const [match, params] = useRoute('/events/:eventId')
-
-    console.log('render sponsors')
-
     return (
         <>
-            <div>Sponsors!</div>
+            <div>Sponsors! TODO</div>
         </>
     )
 }
---
+++ src/events/page/settings/components/WebhooksFields.tsx
@@ -0,0 +1,95 @@
+import * as React from 'react'
+import { useState } from 'react'
+import {
+    Box,
+    Button,
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogContentText,
+    DialogTitle,
+    IconButton,
+    Typography,
+} from '@mui/material'
+import { Control, TextFieldElement, useFieldArray } from 'react-hook-form-mui'
+import { Add, Delete } from '@mui/icons-material'
+import { Event, Webhooks } from '../../../../types'
+
+export type WebhooksFieldsProps = {
+    control: Control<Event, any>
+    isSubmitting: boolean
+}
+type WebhooksWithKey = Webhooks & { id: string }
+export const WebhooksFields = ({ control, isSubmitting }: WebhooksFieldsProps) => {
+    const [selectedWebhook, setSelectedWebhook] = useState<null | Webhooks>(null)
+    const { fields, append, remove } = useFieldArray({
+        control,
+        name: 'webhooks',
+    })
+
+    return (
+        <>
+            <Typography fontWeight="600" mt={2}>
+                Webhooks
+            </Typography>
+
+            <Box paddingLeft={2}>
+                {fields.map((webhook: WebhooksWithKey, index) => (
+                    <Box display="flex" flexDirection="column" key={webhook.id}>
+                        <Box display="flex">
+                            <TextFieldElement
+                                id={webhook.id}
+                                hiddenLabel
+                                name={`webhooks.${index}.url`}
+                                control={control}
+                                variant="filled"
+                                size="small"
+                                margin="dense"
+                                fullWidth
+                                disabled={isSubmitting}
+                            />
+
+                            <IconButton
+                                aria-label="Remove webhook"
+                                onClick={() => {
+                                    remove(index)
+                                }}
+                                edge="end">
+                                <Delete />
+                            </IconButton>
+                        </Box>
+                        <Button
+                            onClick={() => {
+                                setSelectedWebhook(webhook)
+                            }}
+                            size="small">
+                            Last answer: {webhook.lastAnswer ? webhook.lastAnswer.slice(0, 15) + '...' : 'none'}
+                        </Button>
+                    </Box>
+                ))}
+                <IconButton
+                    aria-label="Add webhook"
+                    onClick={() => {
+                        append({
+                            url: '',
+                        })
+                    }}>
+                    <Add />
+                </IconButton>
+            </Box>
+
+            <Dialog open={!!selectedWebhook} onClose={() => setSelectedWebhook(null)}>
+                <DialogTitle>Webhook last answer</DialogTitle>
+                <DialogContent>
+                    <DialogContentText>
+                        <Typography>Url: {selectedWebhook?.url}</Typography>
+                    </DialogContentText>
+                    <pre>{JSON.stringify(selectedWebhook?.lastAnswer, null, 4)}</pre>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={() => setSelectedWebhook(null)}>Close</Button>
+                </DialogActions>
+            </Dialog>
+        </>
+    )
+}
