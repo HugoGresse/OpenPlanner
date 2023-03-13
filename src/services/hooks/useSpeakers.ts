b33a5b07469c92aceb3dfc43b04d1bff8759a479
Hugo Gresse <hugo.gresse@gmail.com>
2023-03-13 20:28:21 +0100

    Fix build

--- src/services/hooks/useSpeakers.ts
+++ src/services/hooks/useSpeakers.ts
@@ -2,6 +2,7 @@ import { UseQueryResult } from 'react-query'
 import { useFirestoreQueryData } from '@react-query-firebase/firestore'
 import { collections } from '../firebase'
 import { Speaker } from '../../types'
+import { DocumentData } from '@firebase/firestore'
 
 export type SpeakersMap = {
     [speakerId: string]: Speaker
@@ -14,12 +15,12 @@ export const useSpeakers = (eventId: string): UseQueryResult<SpeakersMap> => {
     if (sp.data && sp.data.length) {
         return {
             ...sp,
-            data: sp.data.reduce<SpeakersMap>((acc, item: Speaker) => {
-                acc[item.id] = item
+            data: sp.data.reduce<SpeakersMap>((acc, item: DocumentData) => {
+                acc[item.id] = item as Speaker
 
                 return acc
-            }, {}) as UseQueryResult<SpeakersMap>,
-        }
+            }, {}) as SpeakersMap,
+        } as UseQueryResult<SpeakersMap>
     }
 
     return sp as UseQueryResult<SpeakersMap>
