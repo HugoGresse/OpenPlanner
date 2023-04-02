b43a6d134fa425cfbbaadd4a25c25257618c26d3
Hugo Gresse <hugo.gresse@gmail.com>
2023-04-02 23:41:35 +0200

    Add update speaker dialog

--- src/conferencehall/firebase/loadFromConferenceHallUtils.ts
+++ src/conferencehall/firebase/loadFromConferenceHallUtils.ts
@@ -10,7 +10,14 @@ export const loadConferenceHallSpeakers = async (
         acc.push(...chSpeakers)
         return acc
     }, [])
-    const uniqConferenceHallSpeakersIds = [...new Set(chSpeakerIds)]
+    return await loadConferenceHallSpeakersFromIds(chSpeakerIds, progress)
+}
+
+export const loadConferenceHallSpeakersFromIds = async (
+    conferenceHallSpeakerIds: string[],
+    progress: (progress: string) => void
+): Promise<[speakersMap: { [id: string]: ConferenceHallSpeaker }, errors: string[], speakersIds: string[]]> => {
+    const uniqConferenceHallSpeakersIds = [...new Set(conferenceHallSpeakerIds)]
     progress(`Getting ${uniqConferenceHallSpeakersIds.length} ConferenceHall speakers...`)
     const [speakersMap, getSpeakersError] = await getConferenceHallSpeakers(uniqConferenceHallSpeakersIds, progress)
 
--- src/events/page/speakers/EventSpeakers.tsx
+++ src/events/page/speakers/EventSpeakers.tsx
@@ -1,9 +1,12 @@
 import { Box, Button, Card, Container, Typography } from '@mui/material'
 import * as React from 'react'
+import { useState } from 'react'
 import { Event, Speaker } from '../../../types'
 import { FirestoreQueryLoaderAndErrorDisplay } from '../../../components/FirestoreQueryLoaderAndErrorDisplay'
 import { EventSpeakerItem } from './EventSpeakerItem'
 import { useSpeakers } from '../../../services/hooks/useSpeakersMap'
+import { RequireConferenceHallConnections } from '../../../components/RequireConferenceHallConnections'
+import { SpeakersFromConferenceHallUpdaterDialog } from './components/SpeakersFromConferenceHallUpdaterDialog'
 
 export type EventSpeakersProps = {
     event: Event
@@ -11,6 +14,7 @@ export type EventSpeakersProps = {
 }
 export const EventSpeakers = ({ event }: EventSpeakersProps) => {
     const speakers = useSpeakers(event.id)
+    const [updaterDialogOpen, setUpdaterDialogOpen] = useState(false)
 
     if (speakers.isLoading) {
         return <FirestoreQueryLoaderAndErrorDisplay hookResult={speakers} />
@@ -20,6 +24,11 @@ export const EventSpeakers = ({ event }: EventSpeakersProps) => {
         <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
             <Box display="flex" justifyContent="space-between" alignItems="center" marginBottom={1}>
                 <Typography>{speakers.data?.length} speakers</Typography>
+                <RequireConferenceHallConnections event={event}>
+                    <Button onClick={() => setUpdaterDialogOpen(true)}>
+                        Update speakers infos from ConferenceHall
+                    </Button>
+                </RequireConferenceHallConnections>
                 <Button href="/speakers/new" variant="contained">
                     Add speaker
                 </Button>
@@ -29,6 +38,18 @@ export const EventSpeakers = ({ event }: EventSpeakersProps) => {
                     <EventSpeakerItem key={speaker.id} speaker={speaker} />
                 ))}
             </Card>
+            {updaterDialogOpen && (
+                <SpeakersFromConferenceHallUpdaterDialog
+                    event={event}
+                    speakers={speakers.data || []}
+                    isOpen={updaterDialogOpen}
+                    onClose={() => {
+                        setUpdaterDialogOpen(false)
+                        // noinspection JSIgnoredPromiseFromCall
+                        speakers.refetch()
+                    }}
+                />
+            )}
         </Container>
     )
 }
