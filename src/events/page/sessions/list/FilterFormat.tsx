673a62864f6a3372472d79fb175aeb0d9d71c9d7
Hugo Gresse <hugo.gresse@gmail.com>
2023-04-16 15:09:47 +0200

    Add count to format & category filters

--- src/events/page/sessions/list/FilterFormat.tsx
+++ src/events/page/sessions/list/FilterFormat.tsx
@@ -1,14 +1,34 @@
 import { FormControl, InputLabel, MenuItem, Select } from '@mui/material'
 import * as React from 'react'
-import { Event } from '../../../../types'
+import { useEffect } from 'react'
+import { Event, Format, Session } from '../../../../types'
 
 export type FilterSelectProps = {
     event: Event
     selectedFormat: string
     setSelectedFormat: (value: string) => void
+    sessions: Session[]
 }
 
-export const FilterFormat = ({ selectedFormat, setSelectedFormat, event }: FilterSelectProps) => {
+type FormatWithCount = Format & { count: number }
+
+export const FilterFormat = ({ selectedFormat, setSelectedFormat, event, sessions }: FilterSelectProps) => {
+    const [formats, setFormats] = React.useState<FormatWithCount[]>([])
+
+    useEffect(() => {
+        console.log(sessions, event.formats)
+        setFormats(
+            event.formats.map((format) => {
+                return {
+                    id: format.id,
+                    name: format.name,
+                    durationMinutes: format.durationMinutes,
+                    count: sessions.filter((session) => session.format === format.id).length,
+                }
+            })
+        )
+    }, [sessions, event.formats])
+
     return (
         <FormControl fullWidth size="small" margin="normal">
             <InputLabel id="format1">Format</InputLabel>
@@ -19,9 +39,9 @@ export const FilterFormat = ({ selectedFormat, setSelectedFormat, event }: Filte
                 label="Format"
                 onChange={(e) => setSelectedFormat(e.target.value as string)}>
                 <MenuItem value="">All</MenuItem>
-                {event.formats.map((format) => (
+                {formats.map((format) => (
                     <MenuItem key={format.id} value={format.id}>
-                        {format.name}
+                        {format.name} ({format.count} total)
                     </MenuItem>
                 ))}
             </Select>
