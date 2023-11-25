633c6e30fa379aed1f6ef15eeb4afa5e68e979a2
Hugo Gresse <hugo.gresse@gmail.com>
2023-11-25 17:34:57 +0100

    Add reupload params to sponsor

--- functions/src/api/sponsors/sponsors.ts
+++ functions/src/api/sponsors/sponsors.ts
@@ -2,6 +2,7 @@ import { FastifyInstance } from 'fastify'
 import { Static, Type } from '@sinclair/typebox'
 import { apiKeyPlugin } from '../apiKeyPlugin'
 import { SponsorDao } from '../dao/sponsorDao'
+import { uploadBufferToStorage } from '../file/files'
 
 export const Sponsor = Type.Object({
     name: Type.String(),
@@ -12,17 +13,31 @@ export const Sponsor = Type.Object({
 })
 
 export type SponsorType = Static<typeof Sponsor>
+interface IQuerystring {
+    reUploadAssets?: string
+}
 
 export const sponsorsRoutes = (fastify: FastifyInstance, options: any, done: () => any) => {
-    fastify.register(apiKeyPlugin).post<{ Body: SponsorType; Reply: SponsorType }>(
+    fastify.register(apiKeyPlugin).post<{ Querystring: IQuerystring; Body: SponsorType; Reply: SponsorType | string }>(
         '/v1/:eventId/sponsors',
         {
             schema: {
                 tags: ['sponsors'],
                 summary: 'Add one sponsor to an event',
                 body: Sponsor,
+                querystring: {
+                    type: 'object',
+                    additionalProperties: false,
+                    properties: {
+                        reUploadAssets: {
+                            type: 'boolean',
+                            description: 'Download the provided logo to be stored in OpenPlanner storage',
+                        },
+                    },
+                },
                 response: {
                     201: Sponsor,
+                    400: Type.String(),
                 },
                 security: [
                     {
@@ -33,8 +48,37 @@ export const sponsorsRoutes = (fastify: FastifyInstance, options: any, done: () 
         },
         async (request, reply) => {
             const { eventId } = request.params as { eventId: string }
+            const { reUploadAssets } = request.query
 
-            const sponsorId = await SponsorDao.addSponsor(fastify.firebase, eventId, request.body)
+            let logoUrl = request.body.logoUrl
+
+            if (
+                reUploadAssets === 'true' &&
+                request.body.logoUrl &&
+                request.body.logoUrl.startsWith('http') &&
+                request.body.logoUrl.length > 10
+            ) {
+                const response = await fetch(request.body.logoUrl)
+                const arrayBuffer = await response.arrayBuffer()
+
+                const [reUploadSuccess, publicLogoUrl] = await uploadBufferToStorage(
+                    fastify.firebase,
+                    Buffer.from(arrayBuffer),
+                    eventId,
+                    `${Date.now()}`
+                )
+
+                if (!reUploadSuccess) {
+                    return reply.status(400).send('failed to re-upload logo')
+                }
+
+                logoUrl = publicLogoUrl
+            }
+
+            const sponsorId = await SponsorDao.addSponsor(fastify.firebase, eventId, {
+                ...request.body,
+                logoUrl: logoUrl,
+            })
 
             const sponsor = await SponsorDao.getSponsor(fastify.firebase, eventId, request.body.categoryId, sponsorId)
 
