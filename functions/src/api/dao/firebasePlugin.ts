e85f6e9a72f3ceb96ccb35a20982be49ae2f1a5a
Hugo Gresse <hugo.gresse@gmail.com>
2023-12-08 17:04:22 +0100

    Fix firebasePlugin

--- functions/src/api/dao/firebasePlugin.ts
+++ functions/src/api/dao/firebasePlugin.ts
@@ -3,10 +3,12 @@ import fb, { credential } from 'firebase-admin'
 import { FastifyInstance } from 'fastify'
 
 function firebase(fastify: FastifyInstance, options: any, next: () => void) {
+    const cert = process.env.FIREBASE_SERVICE_ACCOUNT
+        ? JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT as string)
+        : undefined
+
     const appConfig = {
-        credential:
-            credential.cert(JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT as string)) ||
-            fb.credential.applicationDefault(),
+        credential: cert ? credential.cert(cert) : fb.credential.applicationDefault(),
     }
 
     const firebaseApp = fb.initializeApp(appConfig)
