def6fedecd3a8af1ccd5cef2dac971c18c04d319
Hugo Gresse <hugo.gresse@gmail.com>
2023-12-11 18:03:48 +0100

    Allow update of faq question through the add route

--- functions/src/api/dao/faqDao.ts
+++ functions/src/api/dao/faqDao.ts
@@ -20,9 +20,10 @@ export class FaqDao {
             })
         }
 
-        const faqId = uuidv4()
+        const faqId = faq.id || uuidv4()
 
-        await db.collection(`events/${eventId}/faq/`).doc(faq.categoryId).collection('items').add({
+        await db.collection(`events/${eventId}/faq/`).doc(faq.categoryId).collection('items').doc(faqId).set({
+            id: faqId,
             question: faq.question,
             answer: faq.answer,
             order: faq.order,
