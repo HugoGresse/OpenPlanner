2d9e4c12436ff6315ca75f2f2743a72385708ac0
Hugo Gresse <hugo.gresse@gmail.com>
2024-06-23 18:23:05 +0200

    Add new menu to convert svg to png when exporting sponsors logo (#127)

--- src/utils/images/downloadImages.ts
+++ src/utils/images/downloadImages.ts
@@ -4,9 +4,14 @@ import { Zip } from '../Zip'
  * Download given images into a single zip file
  * @param zipName
  * @param images
+ * @param convertSvgToPng
  */
-export const downloadImages = async (zipName: string, images: { name: string; url: string }[]) => {
+export const downloadImages = async (
+    zipName: string,
+    images: { name: string; url: string }[],
+    convertSvgToPng = false
+) => {
     const zip = new Zip(zipName)
-    await zip.fetch2Zip(images)
+    await zip.fetch2Zip(images, '', convertSvgToPng)
     await zip.makeZip()
 }
