a82b413d4a229abc3e294545d5a39166beedd4d2
Hugo Gresse <hugo.gresse@gmail.com>
2024-07-27 16:31:16 +0200

    Always return JSON when an error is thrown + details

--- functions/src/api/other/fastifyErrorHandler.ts
+++ functions/src/api/other/fastifyErrorHandler.ts
@@ -1,10 +1,34 @@
 import { FastifyReply, FastifyRequest } from 'fastify'
 import { HttpError } from './Errors'
 
-export const fastifyErrorHandler = (error: Error | HttpError, request: FastifyRequest, reply: FastifyReply) => {
+export const fastifyErrorHandler = (
+    error:
+        | (Error & {
+              code: string
+              statusCode: number
+              validation: any[]
+          })
+        | HttpError,
+    request: FastifyRequest,
+    reply: FastifyReply
+) => {
+    console.error(request.originalUrl, error)
     if (error instanceof HttpError) {
-        reply.status(error.statusCode).send({ error: error.message })
+        reply.header('content-type', 'application/json')
+        reply.status(error.statusCode).send(JSON.stringify({ error: error.message }))
     } else {
-        throw error
+        reply.header('content-type', 'application/json')
+        if (error.code && error.statusCode) {
+            reply.status(error.statusCode).send(
+                JSON.stringify({
+                    error: error.code,
+                    reason: error.toString(),
+                })
+            )
+        } else {
+            reply.status(400).send(JSON.stringify({ error: error.message }))
+
+            console.log('toto')
+        }
     }
 }
