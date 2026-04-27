bb3fdc371242a159c6511d5ec522665c6a015bcf
Hugo Gresse <hugo.gresse@gmail.com>
2026-04-27 11:42:27 +0200

    Revert "Move chat SSE to a dedicated Cloud Function (no Fastify in the response path)"
    This reverts commit 7619099bdc7b9ae5a734280a2e680073d5ca6b5f.

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,4 @@
 import { fastifyFunction } from './api'
-import { chatStream } from './api/chatStreamFunction'
 import { serviceApi } from './serviceApi/serviceApi'
 
-export { fastifyFunction as api, chatStream, serviceApi }
+export { fastifyFunction as api, serviceApi }
